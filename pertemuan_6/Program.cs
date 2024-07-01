ca64d0e1d263ce4fc5d990679a01cccf49f1ec8e
AkhdaanLogics <akhdaan@students.amikom.ac.id>
2024-07-01 20:59:18 +0700

    Add project files.

---
+++ pertemuan_6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Time objTime = new Time(2005, 7, 8, 23, 50, 59);
+            Console.WriteLine("Year : " + objTime.year);
+            Console.WriteLine("Month : " + objTime.month);
+            Console.WriteLine("Date : " + objTime.date);
+            Console.WriteLine("Hour : " + objTime.hour);
+            Console.WriteLine("Minute : " + objTime.minute);
+            Console.WriteLine("Second : " + objTime.second);
+        }
+    }
+}
---
+++ pertemuan_6/Time.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClass
+{
+    class Time
+    {
+        public int year;
+        public int month;
+        public int date;
+        public int hour;
+        public int minute;
+        public int second;
+
+        public Time(int year, int month, int date, int hour, int minute, int second)
+        {
+            this.year = year;
+            this.month = month;
+            this.date = date;
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
+    }
+}
